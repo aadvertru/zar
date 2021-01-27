9f70884d3142743578e5ed862f08d0c15b41a25e
aadvertru <aadvertru@gmail.com>
2021-01-27 20:30:39 +0300

    keep practice

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const el = React.createElement(
+//     'h1',
+//     null,
+//     'Hello World schoen wieder noch ein mal'
+// )
+
+const AppList = () => {
+    const items = ['Item1', 'Item2', "Item3"]
+    return (
+        <ul>
+            {
+                items.map(item => <li>{item}</li>)
+            }
+
+        </ul>
+    )
+}
+
+const AppHeader = () => {
+    return (
+        <h1>Hey</h1>
+    )
+}
+
+const App = () => {
+    return(
+        <>
+            <AppHeader />
+            <AppList />
+            <AppHeader />
+            <AppList />
+        </>
+        )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
